da88e460f9f953e403dedaaf3773620f2f4a65d1
Manuel Perez <Mperez5891@gmail.com>
2020-09-12 01:19:05 -0700

    Create PracticeProblem_P01.py

---
+++ Module 01/PracticeProblem_P01.py
@@ -0,0 +1,37 @@
+# Author:  Manuel Perez
+# Date:    09/11/2020
+#
+# Problem 1 (Easy)
+# Tyler Lockett had the following stat line for 2019.
+# 69 receptions, 82 targets, 1057 receiving yards, 8 
+# receiving touchdowns.
+# Using Python, print Lockett's catch rate and fantasy 
+# points scored for 2019.
+
+########################################################
+# Create a dictionary for Tyler Lockett with his stats
+# for 2019
+player = {
+    "Name": "Tyler Lockett",
+    "Receptions": 69,
+    "Targets": 82,
+    "Receiving yards": 1057,
+    "Receiving Touchdowns": 8
+}
+
+# Calculate Lockett's catch rate
+catches = player["Receptions"]
+targets = player["Targets"]
+lockettsCatchRate = round(catches / targets, 3);
+
+# Calculate Lockett's fantasy points scored
+yards = player["Receiving yards"]
+tds = player["Receiving Touchdowns"]
+lockettsFantasyPoints = catches + (yards * 0.1) + (tds * 6)
+
+# Print Lockett's catch rate and fantasy points scored 
+# for 2019
+print("Tyler Lockett's catch rate was: " 
+      + str(lockettsCatchRate))
+print("Tyler Lockett's fantasy points was: " 
+      + str(lockettsFantasyPoints))
